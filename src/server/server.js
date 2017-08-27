e5b23287012048d0772bd19c36e2058521f8f266
ertrzyiks <mateusz.derks@gmail.com>
2017-08-27 17:49:41 +0200

    Use webpack-dev-middleware

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const path = require('path')
+const webpackMiddleware = require('webpack-dev-middleware')
+const webpack = require('webpack')
+const webpackConfig = require('../../webpack.config')
 
 const app = express()
 
 app.use(express.static('public'))
+
+app.use(webpackMiddleware(webpack(webpackConfig)))
 
 app.get('/', (req, res) => {
   return res.sendFile(path.join(__dirname + '/../index.html'))
